0069ec06085ab8bd8bdbfc88130f4f9d3b61135f
Laura Mafra <imlauramafra@gmail.com>
2026-07-01 07:48:17 -0300

    oi

--- POO/contaBancaria.py
+++ POO/contaBancaria.py
@@ -1,12 +1,31 @@
 class contaBancaria:
-
-#Atributos
-    def __init__(self, titular, numero, saldo = 0):
+    # Atributos
+    def __init__(self, titular, numero, saldo=0):
         self.titular = titular
         self.numero = numero
         self.saldo = saldo
 
-#Métodos
-#depositar
-#sacar
-#extrato
+    # Métodos
+    # depositar
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            print(f"Depósito de R${valor:.2f} realizado com sucesso.")
+        else:
+            print("Valor de depósito inválido. O valor deve ser positivo.")
+
+    # sacar
+    def sacar(self, valor):
+        if valor <= 0:
+            print("Valor de saque inválido. O valor deve ser positivo.")
+        elif valor > self.saldo:
+            print("Saldo insuficiente para realizar o saque.")
+        else:
+            self.saldo -= valor
+            print(f"Saque de R${valor:.2f} realizado com sucesso.")
+
+    # extrato
+    def exibir_extrato(self):
+        print("=" * 20)
+        print(f"conta: {self.numero}") # Corrigido de numero_conta para numero
+        print(f"titular: {self.titular}")
